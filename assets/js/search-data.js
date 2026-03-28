eb456a785b23fb1e81ab645ad76822954d4c4e7d
SunWeihao1226 <59626287+SunWeihao1226@users.noreply.github.com>
2026-03-28 21:41:34 +0000

    Deploying to gh-pages from @ SunWeihao1226/SunWeihao1226.github.io@43acf0dfd6804d8c566dcd97b0453deecd351b5b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -418,16 +418,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-i-presented-my-first-paper-at-ieee-itsc-2025-in-gold-coast-australia",
+          title: 'I presented my first paper at IEEE ITSC 2025 in Gold coast, Australia....',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
